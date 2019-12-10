936c5bb39fdb0ff5e328e3c12649a91d4033c362
Frank Stokes <frank@wearereasonablepeople.com>
2019-12-10 14:01:18 +0100

    Final animation

--- episode-5/index.js
+++ episode-5/index.js
@@ -31,6 +31,9 @@ const writableBytes = new Uint8Array(memory.buffer);
 const cpu = new CPU(MM);
 let i = 0;
 
+// Insert a subroutine for pausing/waiting at the address below
+const waitSubroutineAddress = 0x3100;
+
 const writeCharToScreen = (char, command, position) => {
   writableBytes[i++] = instructions.MOV_LIT_REG;
   writableBytes[i++] = command;
@@ -43,16 +46,77 @@ const writeCharToScreen = (char, command, position) => {
   writableBytes[i++] = position;
 };
 
-writeCharToScreen(' ', 0xff, 0); // Clear the screen
+/*
+ * Use JavaScript to generate the machine code for an animation
+ */
 
-for (let index = 0; index <= 0xff; index++) {
-  const command = index % 2 === 0
-    ? 0x01  // In bold
-    : 0x02; // Regular
-  writeCharToScreen('*', command, index);
+let boldValue = 0;
+
+// Each iteration of the loop draws a different "frame" of animation
+for (let x = 3; x <= 15; x += 2) {
+  boldValue = boldValue === 0 ? 1 : 0;
+
+  // Clear the screen
+  writeCharToScreen(' ', 0xff, 0);
+
+  for (let index = 0; index <= 0xff; index++) {
+    const command = (index % 2 === boldValue)
+      ? 0x01  // In bold
+      : 0x02; // Regular
+    const char = (index % x === 0) ? ' ' : '+';
+    writeCharToScreen(char, command, index);
+  }
+
+  // No arguments for this functional call
+  writableBytes[i++] = instructions.PSH_LIT;
+  writableBytes[i++] = 0x00;
+  writableBytes[i++] = 0x00;
+
+  // Call the pause/wait function
+  writableBytes[i++] = instructions.CAL_LIT;
+  writableBytes[i++] = (waitSubroutineAddress & 0xff00) >> 8;
+  writableBytes[i++] = (waitSubroutineAddress & 0x00ff);
 }
 
-writableBytes[i++] = instructions.HLT;
+// Jump to the start of the code
+writableBytes[i++] = instructions.MOV_LIT_REG;
+writableBytes[i++] = 0x00;
+writableBytes[i++] = 0x00;
+writableBytes[i++] = IP;
 
+//////////// Subroutine for pausing ///////////////
+
+// start writing code at the subroutine address
+i = waitSubroutineAddress;
+
+// R1 is a constant 1, which we add to the accumulator
+writableBytes[i++] = instructions.MOV_LIT_REG;
+writableBytes[i++] = 0;
+writableBytes[i++] = 1;
+writableBytes[i++] = R1;
+
+// Acc starts at zero
+writableBytes[i++] = instructions.MOV_LIT_REG;
+writableBytes[i++] = 0;
+writableBytes[i++] = 0;
+writableBytes[i++] = ACC;
+
+// loopStart is a label for the beginning of this loop
+const loopStart = i;
+
+// Add R1 (1) to the Acc
+writableBytes[i++] = instructions.ADD_REG_REG;
+writableBytes[i++] = R1;
+writableBytes[i++] = ACC;
+
+// if (Acc != 0xccff) then jump tp the start of the loop
+writableBytes[i++] = instructions.JMP_NOT_EQ;
+writableBytes[i++] = 0xcc;
+writableBytes[i++] = 0xff;
+writableBytes[i++] = (loopStart & 0xff00) >> 8;
+writableBytes[i++] = (loopStart & 0x00ff);
+
+// otherwise return from the function
+writableBytes[i++] = instructions.RET;
 
 cpu.run();
